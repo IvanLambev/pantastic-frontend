0d69394c9bd7e067745d8e373f927eb9588c0130
ivan lambev <ivan2007ny@gmail.com>
2025-05-07 21:34:47 +0300

    .

--- my-restaurant-app/vite.config.js
+++ my-restaurant-app/vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
+      external: ['date-fns'],
       output: {
         manualChunks: {
           'react-vendor': ['react', 'react-dom', 'react-router-dom'],
