800ff0e681ea8e38c9146469fd945bec4335ddd9
ivan lambev <ivan2007ny@gmail.com>
2025-07-01 20:37:43 +0300

    fixed sonner

--- my-restaurant-app/src/main.jsx
+++ my-restaurant-app/src/main.jsx
@@ -5,14 +5,15 @@ import { AuthProvider } from "./context/AuthContext"
 import './index.css'
 import App from './App.jsx'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import SonnerToasterRoot from './components/ui/SonnerToasterRoot'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <AuthProvider>
         <App />
+        <SonnerToasterRoot />
     </AuthProvider>
     </BrowserRouter>
-    
   </StrictMode>,
 )
